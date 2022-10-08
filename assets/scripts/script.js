888aa7c3264058b3958405bcc6757516e2df3e01
unknown <ebussman@gmail.com>
2022-10-07 18:07:02 -0700

    Input from the user is successfully stored locally as an array

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,13 @@
+var APIKey = "cb66a065187c471ce473ef1bec816d74";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
 var searchBar = $('#searchBar');
-var searchButton = $('.searchButton')
+var searchButton = $('.searchButton');
+var searchHistory = $('#searchHistory');
+let searchList = [];
+var city = searchList[0];
+
+
 
 // TODO: Weather days function
 // Access weather api from searchbar input
@@ -7,21 +15,42 @@ var searchButton = $('.searchButton')
 // include date, icon for the type of weather, temperature, wind speeds, humidity
 // first datecard should be bigger than the others and include the NAME
 // appends to the page
-function getWeather() {
-    var search = {
-        city: searchBar.val()
-    }
-    localStorage.setItem("Search", JSON.stringify(search));
-    console.log(search);
-    var historyList = [];
-    historyList.textContent(search);
-    console.log(historyList);
-}
 
+// function getApi() {
+  
+//     fetch(queryUrl)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         for (var i = 0; i < data.length; i++) {
+//           var listItem = document.createElement('li');
+//           listItem.textContent = data[i].city;
+//           searchHistory.appendChild(listItem);
+//         }
+//       });
+//   }
 
-// TODO: locally store the search history from the cityname search bar
+// TODO: 
 // display locally stored names up to a certain amount?
 // each name that appears on the page should be a button
 // when that button is pressed, it should access the name it was given and plug that into the weather function 
 
-searchButton.on('click', getWeather);
+
+// creates an object based on what the user puts into the search bar
+// adds that object to our global array so we can make search buttons based on our history
+function saveCityName(event) {
+    event.preventDefault();
+    let search = {
+        city: searchBar.val()
+    } 
+    
+    searchList.unshift(search);
+    console.log(searchList);
+
+    localStorage.setItem("User Searches", JSON.stringify(searchList));
+
+};
+
+
+searchButton.on('click', saveCityName);
